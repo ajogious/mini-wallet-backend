caf513d1ee671ab25c826bfc9c37a0c44662212d
ajogious <ajogious@gmail.com>
2025-11-08 19:43:30 +0100

    Taking the min-wallet to next step ===> Commiting this so that i can know my last commit before update

--- src/main/java/com/miniwallet/MiniWalletApplication.java
+++ src/main/java/com/miniwallet/MiniWalletApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MiniWalletApplication {
+//    Taking the min-wallet to next step ===> Commiting this so that I can know my last commit before update
 	public static void main(String[] args) {
 		SpringApplication.run(MiniWalletApplication.class, args);
+        System.out.println("Taking the min-wallet to next step ===> Commiting this so that i can know my last commit before update");
 	}
 
 }
